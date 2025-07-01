6b4866e5d8dec2169638b187b3587e2422c1e37a
Madhushree K <89026550+madhuu11@users.noreply.github.com>
2025-07-01 16:20:56 +0530

    to get currency code from db

--- src/main/java/com/example/CurrencyExchangeRate/Repository/CurrencyRepository.java
+++ src/main/java/com/example/CurrencyExchangeRate/Repository/CurrencyRepository.java
@@ -2,9 +2,15 @@ package com.example.CurrencyExchangeRate.Repository;
 
 //create a repository interface CurrencyRepository that extends JpaRepository with Currency and Integer as type arguments
 import com.example.CurrencyExchangeRate.Entity.Currency;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CurrencyRepository extends JpaRepository<Currency, Integer> {
+	@Query("SELECT c.currency_code FROM Currency c")
+	List<String> findAllCurrencyCodes();
 }
